2717dbce7a5c31448aa117bab939136dee4aa987
Oscar Wu <oscarwu@mit.edu>
2023-09-20 15:31:48 -0400

    Update: Convert freq str to float and flatten list
    Now freq should be a flat list of floats instead of nested lists. Tests are updated.

--- fastlogfileparser/gaussian/utils/postprocessing.py
+++ fastlogfileparser/gaussian/utils/postprocessing.py
@@ -63,7 +63,7 @@ POSTPROCESSING_FUNCTIONS = {
     "e0_zpe": _str_to_float,
     "gibbs": _str_to_float,
     "frequency_modes": _freq_modes,
-    "frequencies": lambda in_list: [_str_list_to_floats(list(i)) for i in in_list],
+    "frequencies": lambda in_list: [float(i) for sublist in in_list for i in sublist],
     "max_steps": lambda in_list: int(in_list[0]),
     "std_forces": _columns_to_floats,
     "std_xyz": _columns_to_floats,
